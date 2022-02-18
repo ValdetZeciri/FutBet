2c7be99c603133afd63afeaa2ef5b72ff74c360f
Vanja Visekruna <vanja.visekruna05@gmail.com>
2022-02-18 08:21:32 +0100

    hh

--- Anmeldesystem/index.php
+++ Anmeldesystem/index.php
@@ -1,12 +1,51 @@
 <?php 
+
 session_start();
 
 	include("connection.php");
 	include("functions.php");
 
-	$user_data = check_login($con);
+
+	if($_SERVER['REQUEST_METHOD'] == "POST")
+	{
+		//something was posted
+		$user_name = $_POST['user_name'];
+		$password = $_POST['password'];
+
+		if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
+		{
+
+			//read from database
+			$query = "select * from log where username = '$user_name' limit 1";
+			$result = mysqli_query($con, $query);
+
+			if($result)
+			{
+				if($result && mysqli_num_rows($result) > 0)
+				{
+
+					$user_data = mysqli_fetch_assoc($result);
+					
+					if($user_data['password'] === $password)
+					{
+
+						$_SESSION['id'] = $user_data['id'];
+						header("Location: index.php");
+						die;
+					}
+				}
+			}
+			
+			echo "wrong username or password!";
+		}else
+		{
+			echo "wrong username or password!";
+		}
+	}
 
 ?>
+
+
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -18,18 +57,20 @@ session_start();
   <body>
 <div class="login-box">
   <h1>Login</h1>
-  <form action="connect.php" method="post">
+  <form method="post">
     <div class="textbox">
       <i class="fas fa-user"></i>
-      <input type="username" placeholder="Username" class="form-control" id="username" name="username">
+      <input id="text" type="text" name="user_name" class="form-control" placeholder="Username">
     </div>
 
     <div class="textbox">
       <i class="fas fa-lock"></i>
-      <input type="password" placeholder="Password" class="form-control" id="password" name="password">
+      <input id="text" type="password" name="password" placeholder="Password" class="form-control">
     </div>
-    <input type="submit" class="btn" value="Sign in">
-  </form>
+    <input id="button" type="submit" value="Login" class="btn">
+
+	<a href="signup.php" class="btn">Click to Signup</a>
+	</form>
 </div>
   </body>
 </html>
